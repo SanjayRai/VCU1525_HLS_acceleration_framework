392722f162a1eaba48dd0a42b6bd39fef84b79c2
SanjayRai <sanjay.d.rai@gmail.com>
2019-07-12 17:31:06 -0400

    Added debug_bridge IP

--- IP/vivado_project.tcl
+++ IP/vivado_project.tcl
@@ -3,11 +3,8 @@
 source ../device_type.tcl
 create_project project_X project_X -part [DEVICE_TYPE] 
 
-set_property  ip_repo_paths  ../HLS/SDX_ACCL_KERNEL_FP_VECTOR_MULT/vivado_hls_prj/vhls_prj/solution1/impl/ip [current_project]
-update_ip_catalog
-
 add_files -fileset sources_1 -norecurse {
 ./shell/PCIe_Bridge_ICAP_complex/PCIe_Bridge_ICAP_complex.bd
-./role/IP_SDX_ACCL_KERNEL_FP_VECTOR_MULT/HLS_PR_SDX_SRAI/HLS_PR_SDX_SRAI.bd
+./role/debug_bridge_PR/debug_bridge_PR.xci
 }
 
